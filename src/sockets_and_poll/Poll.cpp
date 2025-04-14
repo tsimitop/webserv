82051c5356bbbd72126e7ab1419f9d68dc257278
Thomas Nakas <tnakas@3-C-9.42heilbronn.de>
2025-04-14 14:44:17 +0200

    pushing poll

---
+++ src/sockets_and_poll/Poll.cpp
@@ -0,0 +1,431 @@
+#include "../../inc/sockets_and_poll/Poll.hpp"
+
+// Helper functions
+int setNonBlockingFd(int fd)
+{
+	//not an extra parameter 0
+	int flag = fcntl(fd, F_GETFL);
+	return (fcntl(fd, F_SETFL, flag | O_NONBLOCK));
+}
+
+std::string filteredBuffer(std::string buffer, int& steps_back)
+{
+	std::string new_buffer;
+	int i = -1;
+	while (buffer[++i])
+	{
+		if (isprint(buffer[i]) && !isspace(buffer[i]))
+			new_buffer += buffer[i];
+		else
+			steps_back++;
+	}
+	return new_buffer;
+}
+
+std::string safeNullTermination(int bytes, size_t max_body_ln, std::string buffer)
+{
+	std::string res;
+	for (size_t i = 0; i != max_body_ln; i++)
+		res[i] = buffer[i];
+	if ((size_t)bytes < max_body_ln)
+		res[(size_t)bytes] = '\0';
+	else
+		res[max_body_ln - 1] = '\0';
+	return res;
+};
+
+//Default Constructors
+Poll::Poll():
+poll_success_flag_(YES), 
+port_(8081), 
+host_name_("localhost"), 
+max_body_ln_(1024),
+server_timeout_(10000),
+max_queued_clients_(5), 
+fds_(), 
+server_fd_(0), 
+addr_(),
+config_()
+{};
+
+Poll::Poll(const Poll& other)
+{
+	poll_success_flag_ = other.poll_success_flag_;
+	port_ = other.port_;
+	host_name_ = other.host_name_;
+	max_body_ln_ = other. max_body_ln_;
+	server_timeout_ = other.server_timeout_;
+	max_queued_clients_ = other.max_queued_clients_;
+	fds_ = other.fds_;
+	server_fd_ = other.server_fd_;
+	addr_ = other.addr_;
+	config_ = other.config_;
+};
+
+Poll::Poll(int port, std::string host_name, int max_body_ln, int server_timeout):
+port_(port),
+host_name_(host_name),
+max_body_ln_(max_body_ln),
+server_timeout_(server_timeout)
+{};
+Poll& Poll::operator=(const Poll& other)
+{
+	if (this != &other)
+	{
+		poll_success_flag_ = other.poll_success_flag_;
+		port_ = other.port_;
+		host_name_ = other.host_name_;
+		max_body_ln_ = other. max_body_ln_;
+		server_timeout_ = other.server_timeout_;
+		max_queued_clients_ = other.max_queued_clients_;
+		fds_ = other.fds_;
+		server_fd_ = other.server_fd_;
+		addr_ = other.addr_;
+		config_ = other.config_;
+	}
+	return *this;
+};
+
+Poll::~Poll(){};
+
+//Creating the socket
+int Poll::creatingTheServerSocket()
+{
+	//setting address info the temp and the res(link-list of addrinfo)
+	addrinfo temp, *res;
+	memset(&temp, 0, sizeof(temp));
+	std::string port_char_pointer = std::to_string(port_);
+	temp.ai_family = AF_UNSPEC;
+	temp.ai_socktype = SOCK_STREAM;
+	temp.ai_flags = AI_PASSIVE;
+
+	if (!host_name_.empty() && !std::to_string(port_).empty())
+	{
+		if (getaddrinfo(
+						host_name_.c_str(), 
+						port_char_pointer.c_str(), 
+						&temp, 
+						&res
+						) != 0
+			)
+		{
+			std::cerr << "Error: Get Address Info Failed!\n";
+			freeaddrinfo(res);
+			return NO;
+		};
+	}
+	else
+	{
+		std::cerr << "The port or host_name is empty!\n";
+		return NO;
+	}
+	server_fd_ = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (server_fd_ == -1)
+	{
+		std::cerr << "Error: Socket Failed!\n";
+		freeaddrinfo(res);
+		return NO;
+	}
+	setNonBlockingFd(server_fd_);
+	int opt = 1;
+	if (setsockopt(server_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+	{
+		std::cerr << "Error: SockOpt failed!\n";
+		freeaddrinfo(res);
+		return NO;
+	};
+	if (bind(server_fd_,res->ai_addr,res->ai_addrlen) == -1)
+	{
+		std::cerr << "Error: Bind failed!\n";
+		freeaddrinfo(res);
+		return NO;
+	};
+	listen(server_fd_, max_queued_clients_);
+	fds_.push_back((pollfd){server_fd_, POLLIN, 0});
+	std::cout << "Server is listening to the port: " << port_ << "\n";
+	// freeing the addrinfo
+	freeaddrinfo(res);
+	return YES;
+};
+void Poll::pollingFds()
+{
+	while(YES)
+	{
+		int activity = poll(fds_.data(),fds_.size(), server_timeout_);
+		if (activity == -1)
+		{
+			std::cerr << "Error: Poll failed!\n";
+			poll_success_flag_ = NO;
+			break;
+		}
+		if(fds_[0].revents & POLLIN)
+		{
+			int client_fd = accept(fds_[0].fd, NULL, NULL);
+			if (client_fd == -1)
+				continue ;
+			else
+			{
+				setNonBlockingFd(client_fd);
+				fds_.push_back((pollfd){client_fd, POLLIN, 0});
+				std::cout << "Client Connected to the FD: " << client_fd << "\n";
+			}
+		}
+		for (size_t i = 1; i != fds_.size(); i++)
+		{
+			if (fds_[i].revents & POLLIN)
+			{
+				char buffer[max_body_ln_];
+				int bytes = recv(fds_[i].fd, buffer, sizeof(buffer), 0);
+				if (bytes <= 0)
+				{
+					std::cout << "Client with the FD: " << fds_[i].fd << " Disconnected!\n";
+					close(fds_[i].fd);
+					fds_.erase(fds_.begin() + i);
+					--i;
+				}
+				else
+				{
+					buffer[bytes] = '\0';
+					int steps_back = 0;
+					std::string filtered_buffer = filteredBuffer(buffer, steps_back);
+					if (bytes - steps_back > 0)
+						std::cout << "Client from FD: " << fds_[i].fd << " send the message: " << buffer << "\n";
+					// here will be responce and request
+
+					// here will be responce and request
+					send(fds_[i].fd, &buffer, bytes, 0);		
+				}
+			}
+		}
+	}
+	close(fds_[0].fd);
+};
+
+//how the server is working on the proccess of polling 
+//the sockets is it creating these sockets and waiting to open the page 
+//or is opening it when the request is done
+//for one server
+//I need to go open a port listen accept to the port all the next two
+
+// #include "../../inc/sockets_and_poll/Poll.hpp"
+
+// // Helper functions
+// int setNonBlockingFd(int fd)
+// {
+// 	//not an extra parameter 0
+// 	int flag = fcntl(fd, F_GETFL);
+// 	return (fcntl(fd, F_SETFL, flag | O_NONBLOCK));
+// }
+// std::string filteredBuffer(std::string buffer, int& steps_back)
+// {
+// 	std::string new_buffer;
+// 	int i = -1;
+// 	while (buffer[++i])
+// 	{
+// 		if (isprint(buffer[i]) && !isspace(buffer[i]))
+// 			new_buffer += buffer[i];
+// 		else
+// 			steps_back++;
+// 	}
+// 	return new_buffer;
+// }
+// std::string safeNullTermination(int bytes, size_t max_body_ln, std::string buffer)
+// {
+// 	std::string res;
+// 	for (size_t i = 0; i != max_body_ln; i++)
+// 		res[i] = buffer[i];
+// 	if ((size_t)bytes < max_body_ln)
+// 		res[(size_t)bytes] = '\0';
+// 	else
+// 		res[max_body_ln - 1] = '\0';
+// 	return res;
+// };
+// //Default Constructors
+// Poll::Poll():
+// poll_success_flag_(YES), 
+// port_(8081), 
+// host_name_("localhost"), 
+// max_body_ln_(1024),
+// server_timeout_(10000),
+// max_queued_clients_(5), 
+// fds_(),
+// // flaged_fds_(),
+// server_fd_(0), 
+// addr_(),
+// config_()
+// {};
+// Poll::Poll(const Poll& other)
+// {
+// 	poll_success_flag_ = other.poll_success_flag_;
+// 	port_ = other.port_;
+// 	host_name_ = other.host_name_;
+// 	max_body_ln_ = other. max_body_ln_;
+// 	server_timeout_ = other.server_timeout_;
+// 	max_queued_clients_ = other.max_queued_clients_;
+// 	fds_ = other.fds_;
+// 	// flaged_fds_ = other.flaged_fds_;
+// 	server_fd_ = other.server_fd_;
+// 	addr_ = other.addr_;
+// 	config_ = other.config_;
+// };
+// Poll::Poll(
+// 				int port, 
+// 				std::string host_name, 
+// 				int max_body_ln, 
+// 				int server_timeout
+// 			):
+// port_(port),
+// host_name_(host_name),
+// max_body_ln_(max_body_ln),
+// server_timeout_(server_timeout)
+// {};
+
+// Poll::Poll(const Http& new_config): config_(new_config)
+// {};
+
+// Poll& Poll::operator=(const Poll& other)
+// {
+// 	if (this != &other)
+// 	{
+// 		poll_success_flag_ = other.poll_success_flag_;
+// 		port_ = other.port_;
+// 		host_name_ = other.host_name_;
+// 		max_body_ln_ = other. max_body_ln_;
+// 		server_timeout_ = other.server_timeout_;
+// 		max_queued_clients_ = other.max_queued_clients_;
+// 		fds_ = other.fds_;
+// 		// flaged_fds_ = other.flaged_fds_;
+// 		server_fd_ = other.server_fd_;
+// 		addr_ = other.addr_;
+// 		config_ = other.config_;
+// 	}
+// 	return *this;
+// };
+// Poll::~Poll(){};
+// //Creating the socket
+// int Poll::creatingTheServerSocket()
+// {
+// 	int poll_success_flag_ = YES;
+// 	for (ServerInfo s : config_.servers_)
+// 	{
+// 		addrinfo temp, *res;
+// 		memset(&temp, 0, sizeof(temp));
+// 		std::string port_char_pointer = std::to_string(s.listen_);
+// 		temp.ai_family = AF_UNSPEC;
+// 		temp.ai_socktype = SOCK_STREAM;
+// 		temp.ai_flags = AI_PASSIVE;
+	
+// 		if (!s.server_name_.empty() && !port_char_pointer.empty())
+// 		{
+// 			if (getaddrinfo(
+// 							s.server_name_.c_str(), 
+// 							port_char_pointer.c_str(), 
+// 							&temp, 
+// 							&res
+// 							) != 0
+// 				)
+// 			{
+// 				std::cerr << "Error: Get Address Info Failed!\n";
+// 				freeaddrinfo(res);
+// 				poll_success_flag_ = NO;
+// 				continue;
+// 			};
+// 		}
+// 		else
+// 		{
+// 			std::cerr << "The port or host_name is empty!\n";
+// 			poll_success_flag_ = NO;
+// 			continue;
+// 		}
+// 		server_fd_ = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+// 		if (server_fd_ == -1)
+// 		{
+// 			std::cerr << "Error: Socket Failed!\n";
+// 			freeaddrinfo(res);
+// 			poll_success_flag_ = NO;
+// 			continue;
+// 		}
+// 		setNonBlockingFd(server_fd_);
+// 		int opt = 1;
+// 		if (setsockopt(server_fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+// 		{
+// 			std::cerr << "Error: SockOpt failed!\n";
+// 			freeaddrinfo(res);
+// 			poll_success_flag_ = NO;
+// 			continue;
+// 		};
+// 		if (bind(server_fd_,res->ai_addr,res->ai_addrlen) == -1)
+// 		{
+// 			std::cerr << "Error: Bind failed!\n";
+// 			freeaddrinfo(res);
+// 			poll_success_flag_ = NO;
+// 			continue;
+// 		};
+// 		listen(server_fd_, max_queued_clients_);
+// 		fds_.push_back((pollfd){server_fd_, POLLIN, 0});
+// 		// flaged_fds_.push_back((PollFdWithFlag){{server_fd_, POLLIN, 0}, SERVER});
+// 		std::cout << "Server is listening to the port: " << s.listen_ << "\n";
+// 		// freeing the addrinfo
+// 		freeaddrinfo(res);
+// 	}
+// 	return poll_success_flag_;
+// };
+// void Poll::pollingFds()
+// {
+// 	while(YES)
+// 	{
+// 		int activity = poll(fds_.data(),fds_.size(), server_timeout_); // ?? how I'm taking timeout different per server
+// 		if (activity == -1)
+// 		{
+// 			std::cerr << "Error: Poll failed or Timed out!\n";
+// 			poll_success_flag_ = NO;
+// 			break;
+// 		}
+// 		for (size_t i = 0; i != config_.servers_.size(); i++)
+// 		{
+// 			if (fds_[i].revents & POLLIN)
+// 			{
+// 				int client_fd = accept(fds_[i].fd, NULL, NULL);
+// 				if (client_fd == -1)
+// 					continue ;
+// 				else
+// 				{
+// 					setNonBlockingFd(client_fd);
+// 					fds_.push_back((pollfd){client_fd, POLLIN, 0});
+// 					std::cout << "Client Connected to the FD: " << client_fd << "\n";
+// 				}
+// 			}
+// 		}
+// 		for (size_t i = config_.servers_.size(); i != fds_.size(); i++)
+// 		{
+// 			if (fds_[i].revents & POLLIN)
+// 			{
+// 				char buffer[max_body_ln_]; // how I 'm taking the max body len  per server
+// 				int bytes = recv(fds_[i].fd, buffer, sizeof(buffer), 0);
+// 				if (bytes <= 0)
+// 				{
+// 					std::cout << "Client with the FD: " << fds_[i].fd << " Disconnected!\n";
+// 					close(fds_[i].fd);
+// 					fds_.erase(fds_.begin() + i);
+// 					--i;
+// 				}
+// 				else
+// 				{
+// 					buffer[bytes] = '\0';
+// 					int steps_back = 0;
+// 					std::string filtered_buffer = filteredBuffer(buffer, steps_back);
+// 					if (bytes - steps_back > 0)
+// 						std::cout << "Client from FD: " << fds_[i].fd << " send the message: " << buffer << "\n";
+// 					// here will be responce and request
+
+// 					// here will be responce and request
+// 					send(fds_[i].fd, &buffer, bytes, 0);		
+// 				}
+// 			}
+// 		}
+// 	}
+// 	for (size_t i = 0; i != config_.servers_.size(); i++)
+// 		close(fds_[i].fd);
+// 	while (!fds_.empty())
+// 		fds_.erase(fds_.begin());
+// };
