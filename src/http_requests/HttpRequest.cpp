4444c991b4576cff23ee6125e9a5ca56a0c9ec77
Thomas Nakas <tnakas@2-E-6.42heilbronn.de>
2025-04-24 04:04:42 +0200

    adding sound

--- src/http_requests/HttpRequest.cpp
+++ src/http_requests/HttpRequest.cpp
@@ -473,11 +473,11 @@ const HttpResponse	HttpRequest::getCase(HttpResponse& resp)
 
 const HttpResponse	HttpRequest::deleteCase(HttpResponse& resp)
 {
-	std::filesystem::path current_www_path = this->current_server_.www_path_;
+	std::filesystem::path current_uploads_dir = this->current_server_.uploads_dir_;
 	std::map<int, std::filesystem::path> available_errors = this->current_server_.errors;
-	std::string temp_filename = decodingHexToAscii(this->filename_);
+	// std::string temp_filename = decodingHexToAscii(this->filename_);
 	
-	std::filesystem::path	path_of_file_to_delete = current_www_path / temp_filename;
+	std::filesystem::path	path_of_file_to_delete = current_uploads_dir / this->filename_;
 	std::ifstream file(path_of_file_to_delete);
 	if (!file)
 		resp.createResponse(404, available_errors[404]);
