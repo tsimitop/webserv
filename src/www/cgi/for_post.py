c25db1c3422448b49f2cd3c39e8e35ab299686a8
Thaleia Simitopoulou <tsimitop@2-E-8.42heilbronn.de>
2025-04-26 19:14:22 +0200

    fixed for post py

--- src/www/cgi/for_post.py
+++ src/www/cgi/for_post.py
@@ -3,11 +3,8 @@ import os
 import urllib.parse
 
 def main():
-	print('CONTENT_LENGTH', file=sys.stderr)
-	content_length = int(os.environ.get('CONTENT_LENGTH', 0))
-	print(content_length, file=sys.stderr)
+	# content_length = int(os.environ.get('CONTENT_LENGTH', 0))
 	name = os.environ.get('NAME', 0)
-	print(name, file=sys.stderr)
 
 	print(f"<p>Hello, {name}</p>")
 	print(f"<p>Hello, {name}</p>")
